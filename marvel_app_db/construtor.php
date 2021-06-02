ab9bb4977b4b9efae68c02921062b3fb6cf8a8ae
Nicolas Antonio Smaniotto <49732409+Nicolassmaniotto@users.noreply.github.com>
2021-06-02 16:41:35 -0300

    Update construtor.php

--- marvel_app_db/construtor.php
+++ marvel_app_db/construtor.php
@@ -1,11 +1,16 @@
 <?php
+<?php
+//cria as tabelas, database
 $pos = 'mariadb';
 
 include_once "$pos/createdatabase.php";
 include_once "$pos/conectar.php";
 include_once "$pos/createtable.php";
+
+//pega os valores dos personagems e salva no banco de dados
 header('Location: ./connect3.php')
 
 
 
+?>
 ?>
